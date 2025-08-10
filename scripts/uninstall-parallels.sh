f30dfca2f299331596c333c759e80ca832465231
Alif <arfshl@protonmail.com>
2025-08-10 14:06:25 +0700

    Create uninstall-parallels.sh

---
+++ scripts/uninstall-parallels.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+proot-distro remove portadesx-lxde
+
+rm $PREFIX/etc/proot-distro/portadesx-lxde.sh
+rm /data/data/com.termux/files/usr/bin/portadesx-lxde-cli
+rm /data/data/com.termux/files/usr/bin/portadesx-lxde-gui
+rm /data/data/com.termux/files/usr/bin/portadesx-lxde-help
