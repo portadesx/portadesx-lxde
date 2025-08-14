a52d17b43bd187f2aeb26e6c85b227b5d22d4084
Alif <arfshl@protonmail.com>
2025-08-14 20:22:59 +0700

    Update portadesx-lxde.sh

--- scripts/portadesx-lxde.sh
+++ scripts/portadesx-lxde.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="PortadesX:LXDE"
-TARBALL_URL['aarch64']="7e1d616b61510dfdb37e365b6f1150c92dded580d0b7c50a09563000a5897698"
-TARBALL_SHA256['aarch64']="https://github.com/portadesx/portadesx-lxde/releases/download/24.04-202508091501/portadesx-lxde-2404.tar.xz"
+TARBALL_URL['aarch64']="c8fdce44768bd6c1dce531d22a08b1e2d26f683ae55ccec9ea9bc0ea67746873"
+TARBALL_SHA256['aarch64']="https://github.com/portadesx/portadesx-lxde/releases/download/noble-aarch64/portadesx-lxde.tar.xz"
 distro_setup() {
         run_proot_cmd ln -s /storage/emulated/0/ /home/portadesx/Desktop/android_files
 }
